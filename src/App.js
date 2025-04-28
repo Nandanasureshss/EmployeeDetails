5c8e9ba3a2674cc262fba7a6f8e1d5a25a6d7679
vinita132r <vinitar132@gmail.com>
2025-04-28 23:08:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Registration from './User/Registration';
-// import NewLandingPage from './NewLandingPage';
+import Userlist from './User/Userlist';
+import Userprofile from './User/Userprofile';
+// import Homepg from './User/Homepg';
+import Login from './User/Login';
+import NewLandingPage from './NewLandingPage';
+// import { useNavigate } from 'react-router-dom';
+import Userhome from './User/Userhome';
+import Admindashboard from './Admin/Admindashboard';
+import AdminNotification from './Admin/AdminNotification';
+import Adminuserlist from './Admin/Adminuserlist';
+import Adminviewuserprofile from './Admin/Adminviewuserprofile';
+import Adminlogin from './Admin/Adminlogin';
+import Userviewoneprof from './User/Userviewoneprof';
+
 // import Navbar from './Navbar';
 function App() {
   return (
     <div>
-      <Registration/>
-{/* <NewLandingPage/> */}
-{/* <Navbar/> */}
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NewLandingPage/>}/>
+        {/* <Route path="/homepg" element={<Homepg/>}/> */}
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Registration/>}/>
+        <Route path="/userhome" element={<Userhome/>}/>
+        <Route path="/userlist" element={<Userlist/>}/>
+        <Route path="/userprofile/:id" element={<Userprofile/>}/>
+        <Route path="/userviewone/:id" element={<Userviewoneprof/>}/>
+
+
+        <Route path="/adminlogin" element={<Adminlogin/>}/>
+        <Route path="/adminhome" element={<Admindashboard/>}/>
+        <Route path="/adminnotify" element={<AdminNotification/>}/>
+        <Route path="/adminuserlist" element={<Adminuserlist/>}/>
+        <Route path="/adminviewuser" element={<Adminviewuserprofile/>}/>
+
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/NewLandingPage.jsx
+++ src/NewLandingPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './NewLandingPage.css';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+
+import homepic from '../src/asserts/emphome.webp'
 
 function NewLandingPage() {
   return (
     <>
       <Navbar/>
       <div className="bodylandingpage">
+        <img src={homepic} alt="emphome" id='emphomebgimg'/>
+        <center><div id="homeheading">Welcome to Employee Management System...</div></center>
       </div>
     </>
   );
